49030c04178bc68ea2f5b2853e956167d0a8f315
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-03-16 20:44:55 +0530

    added scrolling effect

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scrool-to');
+const section1 = document.querySelector('#section--1');
+const navLinks = document.querySelector('.nav__links');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -27,5 +30,28 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
+    }
+});
+
+btnScrollTo.addEventListener('click', (e) => {
+
+    //old way
+    // const s1Coords = section1.getBoundingClientRect();
+    // window.scrollTo({
+    //     left: s1Coords.left + window.pageXOffset,
+    //     top: s1Coords.top + window.pageYOffset,
+    //     behavior: 'smooth'
+    // });
+
+    //New way
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+navLinks.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
     }
 });
