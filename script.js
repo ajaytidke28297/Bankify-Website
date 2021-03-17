5b36a84d6377d4156c4db59b8e5c860c5406f675
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-03-17 19:42:33 +0530

    operations section & Menu hover implementation

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scrool-to');
 const section1 = document.querySelector('#section--1');
 const navLinks = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('.operations__tab');
+const contents = document.querySelectorAll('.operations__content');
+const tabContainer = document.querySelector('.operations__tab-container');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -55,3 +59,34 @@ navLinks.addEventListener('click', (e) => {
         document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
     }
 });
+
+//Operations tab implementation
+tabContainer.addEventListener('click', (e) => {
+    const activeTab = e.target.closest('.operations__tab');
+
+    if (!activeTab) return;
+
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    contents.forEach(c => c.classList.remove('operations__content--active'));
+
+
+    activeTab.classList.add('operations__tab--active');
+    document.querySelector(`.operations__content--${activeTab.dataset.tab}`).classList.add('operations__content--active');
+});
+
+//Handle hover effect
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img')
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
